a29fe462d1195cc74f4249859ef4addd70022636
vasiliyChobot <i@vchobot.ru>
2023-01-10 23:29:49 +0300

    added gitattributes

--- src/main/java/hexlet/code/Rollbar.java
+++ src/main/java/hexlet/code/Rollbar.java
@@ -1,19 +1,18 @@
 package hexlet.code;
 
-import com.rollbar.notifier.Rollbar;
 import com.rollbar.notifier.config.Config;
 
 import static com.rollbar.spring.webmvc.RollbarSpringConfigBuilder.withAccessToken;
 
-public class Application implements AutoCloseable {
-    private Rollbar rollbar;
+public class Rollbar implements AutoCloseable {
+    private com.rollbar.notifier.Rollbar rollbar;
 
-    public Application() {
+    public Rollbar() {
         Config config = withAccessToken("8425592a0bc94e2ab85389df2f793ad7")
                 .environment("production")
                 .codeVersion("1.0.0")
                 .build();
-        this.rollbar = Rollbar.init(config);
+        this.rollbar = com.rollbar.notifier.Rollbar.init(config);
     }
 
     @Override
@@ -22,7 +21,7 @@ public class Application implements AutoCloseable {
     }
 
     public static void main(String[] args) throws Exception {
-        try (Application app = new Application()) {
+        try (Rollbar app = new Rollbar()) {
             app.execute();
         }
     }
